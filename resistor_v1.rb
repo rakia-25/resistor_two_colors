90ed5cc2561e219c6fbd9134eb2361277402dba3
Rakia <soumaila.rakia@yahoo.fr>
2025-03-08 11:22:08 +0100

    create file readme

--- resistor_v1.rb
+++ resistor_v1.rb
@@ -1,16 +1,16 @@
-hash={ 'black'=>'0','brown'=>'1','red'=>'2','orange'=>'3','yellow'=>'4','green'=>'5','blue'=>'6','violet'=>'7','grey'=>'8','white'=>'9'}
+color_mapping={ 'black'=>'0','brown'=>'1','red'=>'2','orange'=>'3','yellow'=>'4','green'=>'5','blue'=>'6','violet'=>'7','grey'=>'8','white'=>'9'}
 puts "please choose a color band(separate colors with dashes)"
 puts ""
-color_value=[]
 input=gets.chomp
-band_colour=input.downcase.split('-')
-band_colour.each do |color|
-  if hash.include?(color)
-      color_value.push(hash[color])
+band_color_input=input.downcase.split('-')
+band_color_input_value=[]
+band_color_input.each do |color|
+  if color_mapping.include?(color)
+      band_color_input_value.push(color_mapping[color])
   else
     puts "the color is not available"
   end
 end
-t=[color_value[0],color_value[1]]
-puts "#{t.join}"
+t=[band_color_input_value[0],band_color_input_value[1]]
+puts "the resistance value is #{t.join}"
 
